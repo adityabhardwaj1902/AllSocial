df1e69b7af0151fc521d2355ebb00999295d37ea
Aditya <adityabhardwaj1902@gmail.com>
2022-09-05 14:14:04 +0530

    set up layouts

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 
 const port = 8000;
 
+const expressLayouts = require("express-ejs-layouts");
+
+app.use(expressLayouts);
 // use express router
 app.use("/", require("./routes"));
+
+// set up template engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.listen(port, function (err) {
   if (err) {
